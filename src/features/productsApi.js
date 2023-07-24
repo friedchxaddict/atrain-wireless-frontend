b5806416c724272b30fcaf29c60825dfdd515ee3
@adrianfontanilla <@adrianfontanilla>
2023-07-23 23:06:23 -0500

    add backend deployed address

--- src/features/productsApi.js
+++ src/features/productsApi.js
@@ -1,13 +1,15 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const productsApi = createApi({
-    reducerPath: "productsApi",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
-    endpoints: (builder) => ({
-        getAllProducts: builder.query({
-            query: () => "products",
-        }),
+  reducerPath: "productsApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://atrain-backend.onrender.com",
+  }),
+  endpoints: (builder) => ({
+    getAllProducts: builder.query({
+      query: () => "products",
     }),
+  }),
 });
 
-export const { useGetAllProductsQuery } = productsApi
+export const { useGetAllProductsQuery } = productsApi;
