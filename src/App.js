116f1d19aba3016712c9c35d03bd18d04ac6e709
@adrianfontanilla <@adrianfontanilla>
2023-07-23 22:08:37 -0500

    Initial push for phone e-commerce example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import "react-toastify/dist/ReactToastify.css";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+import NavBar from './components/NavBar';
+import Cart from './components/Cart';
+import Home from './components/Home';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <ToastContainer />
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />         
+          <Route path="*" element={<NotFound />} />
+          {/*<Navigate to="not-found" />*/}        
+        </Routes>
+      </BrowserRouter>    
     </div>
   );
 }
