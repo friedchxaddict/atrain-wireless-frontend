8343e9b0075ca9d283893a237cd43d84a0612859
@adrianfontanilla <@adrianfontanilla>
2023-09-13 21:42:54 -0500

    change backend url temporarily

--- src/features/productsSlice.js
+++ src/features/productsSlice.js
@@ -11,7 +11,7 @@ export const productsFetch = createAsyncThunk(
   "products/productsFetch",
   async () => {
     const response = await axios.get(
-      "https://friendly-dove-apron.cyclic.cloud/products"
+      "https://atrain-backend.onrender.com/products" //"https://friendly-dove-apron.cyclic.cloud/products"
     );
     console.log(response);
     return response?.data;
