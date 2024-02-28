9e30f49116d6d6e4ad1d7112953ffa0f51ca5c77
@adrianfontanilla <adrianfontanilla@gmail.com>
2024-02-27 22:48:40 -0600

    add spinner

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,43 +1,50 @@
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router";
-import { addToCart } from "../features/cartSlice";
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router';
+import { addToCart } from '../features/cartSlice';
+import SpinnerLoader from './SpinnerLoader';
 
-import { useGetAllProductsQuery } from "../features/productsApi";
+import { useGetAllProductsQuery } from '../features/productsApi';
 
 const Home = () => {
-    const {data, error, isLoading} = useGetAllProductsQuery();
-    const dispatch = useDispatch();
-    const navigate = useNavigate()
+  const { data, error, isLoading } = useGetAllProductsQuery();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const handleAddToCart = (product) => {
-        dispatch(addToCart(product))
-        navigate("/cart");
-    }
+  const handleAddToCart = (product) => {
+    dispatch(addToCart(product));
+    navigate('/cart');
+  };
 
-    return <div className="home-container">
-        { isLoading ? (
-            <p>Loading...</p> 
-        ) : error ? (
-          <p>An error occurred..</p> 
-        ) : (
-          <>
+  return (
+    <div className="home-container">
+      {isLoading ? (
+        <SpinnerLoader>
+          <p>Loading...</p>
+        </SpinnerLoader>
+      ) : error ? (
+        <p>An error occurred..</p>
+      ) : (
+        <>
           <h2>New Arrivals</h2>
           <div className="products">
-              {data?.map((product) => (
+            {data?.map((product) => (
               <div key={product.id} className="product">
                 <h3>{product.name}</h3>
                 <img src={product.image} alt={product.name} />
                 <div className="details">
-                    <span>{product.desc}</span>
-                    <span className="price">{product.price}</span>                   
-                </div>  
-                <button onClick = {() => handleAddToCart(product)}>Add To Cart</button>
-              </div> 
-              ))}
+                  <span>{product.desc}</span>
+                  <span className="price">{product.price}</span>
+                </div>
+                <button onClick={() => handleAddToCart(product)}>
+                  Add To Cart
+                </button>
+              </div>
+            ))}
           </div>
-          </>
-        )}
+        </>
+      )}
     </div>
+  );
 };
- 
+
 export default Home;
---
+++ src/components/SpinnerLoader.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Loader from 'react-loader-spinner';
+
+const SpinnerLoader = () => {
+  return (
+    <Loader type="Puff" color="black" height={100} width={100} timeout={3000} />
+  );
+};
+
+export default SpinnerLoader;
